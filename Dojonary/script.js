59bc6442022af73f826c67d2aaceeea0e9010a14
bashar nowar <basharnowar@gmail.com>
2026-09-08 21:16:19 +0300

    Dojonary

---
+++ Dojonary/script.js
@@ -0,0 +1,10 @@
+function turnLogout(btn) {
+  btn.textContent = "Logout";  
+}
+
+function hide(element){
+  element.style.display= "none";
+}
+function likeNinja() {
+  alert("ninja was liked");
+}
