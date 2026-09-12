f79063549c105453d187e88a624ffabb0b342e59
bashar nowar <basharnowar@gmail.com>
2026-09-12 15:28:06 +0300

    Likes

--- JS/DojoWeather/script.js
+++ JS/DojoWeather/script.js
@@ -6,3 +6,23 @@ function hide(){
     var banner = document.querySelector(".cookie-banner")
     banner.remove();
 }
+
+function anything(eleem) {
+    var unit = eleem.value; 
+
+    var highs = document.querySelectorAll(".high");
+    var lows = document.querySelectorAll(".low");
+
+    var highC = [24, 27, 21, 26];
+    var lowC = [18, 19, 16, 21];
+
+    for (var i = 0; i < highs.length; i++) {
+        if (unit === "C") {
+            highs[i].innerText = highC[i] + "°";
+            lows[i].innerText = lowC[i] + "°";
+        } else {
+            highs[i].innerText = Math.round(highC[i] * 9 / 5 + 32) + "°";
+            lows[i].innerText = Math.round(lowC[i] * 9 / 5 + 32) + "°";
+        }
+    }
+}
---
+++ JS/Likes/scripte.js
@@ -0,0 +1,13 @@
+// var like1 = document.querySelector("#neil");
+
+// function increase(){
+//     let int = parseInt(like1.innerText)
+//     var num = int + 1
+//     like1.innerText = num + " like(s)"
+// }
+
+function increaseLike(button) {
+    const label = button.previousElementSibling;
+    const num = parseInt(label.innerText) + 1;
+    label.innerText = num + " like(s)";
+}
