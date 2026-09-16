66ea8f7007e002a757093470b945d2d01b1b4987
bashar nowar <basharnowar@gmail.com>
2026-09-16 16:24:39 +0300

    DojoWeather

--- JS/DojoWeather/script.js
+++ JS/DojoWeather/script.js
@@ -1,33 +1,40 @@
 function show(element){
     alert("Showing weather for " + element.innerText)
-}
-
-function hide(){
-    var banner = document.querySelector(".cookie-banner")
-    banner.remove();
-}
-
-function change(){
-    var CF = document.querySelector('.high');
-    CF.innerText = "70";
-}
-
-// function anything(eleem) {
-//     var unit = eleem.value; 
-
-//     var highs = document.querySelectorAll(".high");
-//     var lows = document.querySelectorAll(".low");
-
-//     var nums = document.querySelector("p span");
     
+}
 
-//     for (var i = 0; i < highs.length; i++) {
-//         if (unit === "C") {
-//             highs[i].innerText = highC[i] + "°";
-//             lows[i].innerText = lowC[i] + "°";
-//         } else {
-//             highs[i].innerText = Math.round(highC[i] * 9 / 5 + 32) + "°";
-//             lows[i].innerText = Math.round(lowC[i] * 9 / 5 + 32) + "°";
-//         }
-//     }
+// function show(element) {
+//     var show = document.querySelector(".show")
+//     show.remove();
 // }
+
+// function hide(){
+//     var banner = document.querySelector(".cookie-banner")
+//     banner.remove();
+// }
+
+// function change(){
+//     var CF = document.querySelector('.high');
+//     CF.innerText = "70";
+// }
+
+function change(eleem) {
+    var unit = eleem.value; 
+
+    var highs = document.querySelectorAll(".high");
+    var lows = document.querySelectorAll(".low");
+
+    for (var i = 0; i < highs.length; i++) {
+        var highC = Number(highs[i].dataset.c);
+        var lowC = Number(lows[i].dataset.c);
+
+        if (unit === "C") {
+            highs[i].innerText = highC + "°";
+            lows[i].innerText = lowC + "°";
+        }
+        if (unit === "F") {
+            highs[i].innerText = Math.round(highC * 9 / 5 + 32) + "°";
+            lows[i].innerText = Math.round(lowC * 9 / 5 + 32) + "°";
+        }
+    }
+}
